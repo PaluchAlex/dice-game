fc37215809fa1ef1b8022192202b0d5c6136e0c0
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2023-04-04 17:04:59 +0300

    add Roll button

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-function test() {
-  console.log("yes!");
+function playGame(params) {
+    
+    //dice1
+    var randomNr1 = Math.floor((Math.random()*6)+1);
+    var baseString = "images/dice";
+    var imgAddres = baseString.concat(randomNr1.toString(), ".png");
+    document.querySelector(".img1").setAttribute("src", imgAddres);
+
+    //dice2
+    var randomNr2 = Math.floor((Math.random()*6)+1);
+    var imgAddres2 = baseString.concat(randomNr2.toString(), ".png");
+    document.querySelector(".img2").setAttribute("src", imgAddres2);
+
+    if (randomNr1>randomNr2) {
+        document.querySelector("h1").textContent = "🚩 Player 1 Won!";
+        document.querySelectorAll("p")[0].innerHTML = "🚩Player 1🚩"
+    } else if (randomNr1<randomNr2) {
+        document.querySelector("h1").textContent ="Player 2 Won! 🚩";
+        document.querySelectorAll("p")[1].innerHTML = "🚩Player 2🚩"
+    } else {  
+        document.querySelector("h1").textContent = "🚩 Draw! 🚩";
+    }
 }
-test();
+
+$(".play-btn").on("click", function (btn) {
+    
+    playGame();
 
 
-//dice1
-var randomNr1 = Math.floor((Math.random()*6)+1);
-var baseString = "images/dice";
-var imgAddres = baseString.concat(randomNr1.toString(), ".png");
-document.querySelector(".img1").setAttribute("src", imgAddres);
-
-//dice2
-var randomNr2 = Math.floor((Math.random()*6)+1);
-var imgAddres2 = baseString.concat(randomNr2.toString(), ".png");
-document.querySelector(".img2").setAttribute("src", imgAddres2);
-
-if (randomNr1>randomNr2) {
-    document.querySelector("h1").textContent = "🚩 Player 1 Won! ❌";
-    document.querySelectorAll("p")[0].innerHTML = "🚩Player 1🚩"
-} else if (randomNr1<randomNr2) {
-    document.querySelector("h1").textContent ="❌ Player 2 Won! 🚩";
-    document.querySelectorAll("p")[1].innerHTML = "🚩Player 2🚩"
-} else {  
-    document.querySelector("h1").textContent = "🚩 Draw! 🚩";
-}
+})
 
 
 
